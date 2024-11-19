ff4512c682a43ab8b45c0f713b879be553b637d8
Madhavan <127614873+R-Madhavan@users.noreply.github.com>
2024-11-19 13:19:53 +0530

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -3,33 +3,31 @@ import requests
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
 SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/2d11c96855f2b8cf43f6708d2261f4fe/flightDeals/prices"
 
-class DataManager: import os
-import requests
-from requests.auth import HTTPBasicAuth
-from dotenv import load_dotenv
 
-load_dotenv()
-
-SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/2d11c96855f2b8cf43f6708d2261f4fe/flightDeals/prices"
-
-class DataManager:            #This class is responsible for talking to the Google Sheet.
+class DataManager:
 
     def __init__(self):
-        self._user = os.environ["SHEETY_USERNAME"]
+        self._user = os.environ["SHEETY_USRERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
         self._authorization = HTTPBasicAuth(self._user, self._password)
         self.destination_data = {}
 
     def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
         response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
         data = response.json()
         self.destination_data = data["prices"]
+        # Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
         return self.destination_data
 
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
     def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {
@@ -40,32 +38,6 @@ class DataManager:            #This class is responsible for talking to the Goog
             response = requests.put(
                 url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
                 auth=self._authorization,
-                json=new_data,
-            )
-            print(response.text)
-
-    def __init__(self):
-        self._user = os.environ["SHEETY_USERNAME"]
-        self._password = os.environ["SHEETY_PASSWORD"]
-        self._authorization = HTTPBasicAuth(self._user, self._password)
-        self.destination_data = {}
-
-    def get_destination_data(self):
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
-        data = response.json()
-        self.destination_data = data["prices"]
-        return self.destination_data
-
-    def update_destination_codes(self):
-        for city in self.destination_data:
-            new_data = {
-                "price": {
-                    "iataCode": city["iataCode"]
-                }
-            }
-            response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
-                auth=self._authorization,
-                json=new_data,
+                json=new_data
             )
             print(response.text)
