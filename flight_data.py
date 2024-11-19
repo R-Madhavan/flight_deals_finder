c7fc8e3abc004f20719505634ed037e8a3f06e49
Madhavan <127614873+R-Madhavan@users.noreply.github.com>
2024-11-19 13:20:13 +0530

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -1,8 +1,10 @@
+
 class FlightData:
 
     def __init__(self, price, origin_airport, destination_airport, out_date, return_date):
         """
         Constructor for initializing a new flight data instance with specific travel details.
+
         Parameters:
         - price: The cost of the flight.
         - origin_airport: The IATA code for the flight's origin airport.
@@ -17,8 +19,26 @@ class FlightData:
         self.return_date = return_date
 
 def find_cheapest_flight(data):
+    """
+    Parses flight data received from the Amadeus API to identify the cheapest flight option among
+    multiple entries.
+
+    Args:
+        data (dict): The JSON data containing flight information returned by the API.
+
+    Returns:
+        FlightData: An instance of the FlightData class representing the cheapest flight found,
+        or a FlightData instance where all fields are 'NA' if no valid flight data is available.
+
+    This function initially checks if the data contains valid flight entries. If no valid data is found,
+    it returns a FlightData object containing "N/A" for all fields. Otherwise, it starts by assuming the first
+    flight in the list is the cheapest. It then iterates through all available flights in the data, updating
+     the cheapest flight details whenever a lower-priced flight is encountered. The result is a populated
+     FlightData object with the details of the most affordable flight.
+    """
+
     # Handle empty data if no flight or Amadeus rate limit exceeded
-    if data is None or not data['data']:
+    if data is None or not data["data"]:
         print("No flight data")
         return FlightData("N/A", "N/A", "N/A", "N/A", "N/A")
 
@@ -45,4 +65,3 @@ def find_cheapest_flight(data):
             print(f"Lowest price to {destination} is £{lowest_price}")
 
     return cheapest_flight
-  
